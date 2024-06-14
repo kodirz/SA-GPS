ca059ea9a319b53b512d44ebc937d3958a3f9a6b
kodirz <sultonabdul12345@gmail.com>
2024-06-15 04:10:56 +0700

    Bug dibagian search

--- fake_gps/ios/Runner/AppDelegate.swift
+++ fake_gps/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCndo-t86647HqyI7loCuW-OnIRsTheNTM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- fake_gps/lib/main.dart
+++ fake_gps/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:google_maps_webservice/places.dart';
+// import 'package:geocoding/geocoding.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,12 +32,69 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
-
-  final LatLng _center =
+  final LatLng _initialCenter =
       const LatLng(-7.795580, 110.369490); // Koordinat Yogyakarta
+  LatLng _currentCenter = const LatLng(-7.795580, 110.369490);
+  final Set<Marker> _markers = {};
+  static const kGoogleApiKey = "YOUR_API_KEY";
+  final Mode _mode = Mode.overlay;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    _addMarker(_initialCenter);
+  }
+
+  void _addMarker(LatLng position) {
+    setState(() {
+      _markers.clear(); // Hapus marker sebelumnya jika ada
+      _markers.add(
+        Marker(
+          markerId: const MarkerId('center_marker'),
+          position: position,
+          infoWindow: const InfoWindow(
+            title: 'Yogyakarta',
+            snippet: 'Ini adalah lokasi di Yogyakarta',
+          ),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        ),
+      );
+      _currentCenter = position;
+    });
+  }
+
+  void _onMapTapped(LatLng position) {
+    _addMarker(position);
+  }
+
+  Future<void> _handlePressButton() async {
+    Prediction? p = await PlacesAutocomplete.show(
+      context: context,
+      apiKey: kGoogleApiKey,
+      mode: _mode,
+      language: "en",
+      components: [Component(Component.country, "id")],
+    );
+
+    displayPrediction(p!, mapController);
+  }
+
+  Future<void> displayPrediction(
+      Prediction p, GoogleMapController controller) async {
+    if (p != null) {
+      GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
+      PlacesDetailsResponse detail =
+          await _places.getDetailsByPlaceId(p.placeId!);
+      final lat = detail.result.geometry!.location.lat;
+      final lng = detail.result.geometry!.location.lng;
+
+      _addMarker(LatLng(lat, lng));
+      controller.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(lat, lng),
+          zoom: 14.0,
+        ),
+      ));
+    }
   }
 
   @override
@@ -42,19 +102,27 @@ class _MapScreenState extends State<MapScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Fake GPS'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: _handlePressButton,
+          ),
+        ],
       ),
       body: GoogleMap(
         onMapCreated: _onMapCreated,
         initialCameraPosition: CameraPosition(
-          target: _center,
+          target: _initialCenter,
           zoom: 11.0,
         ),
+        markers: _markers,
+        onTap: _onMapTapped,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           mapController.animateCamera(CameraUpdate.newCameraPosition(
             CameraPosition(
-              target: _center,
+              target: _currentCenter,
               zoom: 14.0,
             ),
           ));
