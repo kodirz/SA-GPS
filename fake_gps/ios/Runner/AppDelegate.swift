4451ea939e017bde75e6b8db9dc2cc2c4ca30dae
kodirz <sultonabdul12345@gmail.com>
2024-06-16 01:06:30 +0700

    commat cammit

--- fake_gps/ios/Runner/AppDelegate.swift
+++ fake_gps/ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCndo-t86647HqyI7loCuW-OnIRsTheNTM")
+    GMSServices.provideAPIKey("AIzaSyAtINayJ-zJovfZkv6jPjEu7iECO5pOCzU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- fake_gps/lib/main.dart
+++ fake_gps/lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:google_maps_webservice/places.dart';
-// import 'package:geocoding/geocoding.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fake GPS',
+      title: 'KODIR GPS',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -32,55 +33,112 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
-  final LatLng _initialCenter =
-      const LatLng(-7.795580, 110.369490); // Koordinat Yogyakarta
-  LatLng _currentCenter = const LatLng(-7.795580, 110.369490);
   final Set<Marker> _markers = {};
-  static const kGoogleApiKey = "YOUR_API_KEY";
+  static const kGoogleApiKey = "AIzaSyAtINayJ-zJovfZkv6jPjEu7iECO5pOCzU";
   final Mode _mode = Mode.overlay;
+  List<LatLng> _locations = []; // Daftar lokasi yang dipilih oleh pengguna
+
+  @override
+  void dispose() {
+    mapController.dispose();
+    super.dispose();
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
-    _addMarker(_initialCenter);
+    _getCurrentLocation(); // Panggil fungsi untuk mendapatkan lokasi saat ini
   }
 
   void _addMarker(LatLng position) {
     setState(() {
-      _markers.clear(); // Hapus marker sebelumnya jika ada
       _markers.add(
         Marker(
-          markerId: const MarkerId('center_marker'),
+          markerId: MarkerId('${position.latitude}_${position.longitude}'),
           position: position,
           infoWindow: const InfoWindow(
-            title: 'Yogyakarta',
-            snippet: 'Ini adalah lokasi di Yogyakarta',
+            title: 'Lokasi Terpilih',
+            snippet: 'Ini adalah lokasi yang Anda pilih',
           ),
           icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
         ),
       );
-      _currentCenter = position;
+      _locations.add(position); // Tambah lokasi ke dalam daftar
     });
   }
 
-  void _onMapTapped(LatLng position) {
+  void _onMapTapped(LatLng position) async {
     _addMarker(position);
+
+    // Mendapatkan alamat dari koordinat yang ditap
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    if (placemarks.isNotEmpty) {
+      Placemark place = placemarks.first;
+      print('Alamat: ${place.street}, ${place.locality}, ${place.country}');
+    }
+
+    // Animasikan kamera peta ke lokasi yang ditap
+    mapController.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: position,
+        zoom: 14.0,
+      ),
+    ));
+  }
+
+  void _startRoute() {
+    if (_locations.isNotEmpty) {
+      // Menghapus semua marker kecuali yang terakhir
+      List<Marker> newMarkers = [_markers.last];
+      setState(() {
+        _markers.clear();
+        _markers.addAll(newMarkers);
+      });
+
+      // Animasi kamera peta ke lokasi terakhir dalam _locations
+      LatLng lastLocation = _locations.last;
+      mapController.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: lastLocation,
+          zoom: 14.0,
+        ),
+      ));
+
+      // Tampilkan snackbar notifikasi
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.blue,
+          content: const Text('Lokasi telah diperbarui.'),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    }
   }
 
   Future<void> _handlePressButton() async {
-    Prediction? p = await PlacesAutocomplete.show(
-      context: context,
-      apiKey: kGoogleApiKey,
-      mode: _mode,
-      language: "en",
-      components: [Component(Component.country, "id")],
-    );
+    try {
+      Prediction? p = await PlacesAutocomplete.show(
+        context: context,
+        apiKey: kGoogleApiKey,
+        mode: _mode,
+        language: "en",
+        components: [Component(Component.country, "id")],
+      );
 
-    displayPrediction(p!, mapController);
+      if (p != null) {
+        await displayPrediction(p, mapController);
+      }
+    } catch (e) {
+      print("Error during search: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error: $e")),
+      );
+    }
   }
 
   Future<void> displayPrediction(
       Prediction p, GoogleMapController controller) async {
-    if (p != null) {
+    try {
       GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
       PlacesDetailsResponse detail =
           await _places.getDetailsByPlaceId(p.placeId!);
@@ -94,6 +152,41 @@ class _MapScreenState extends State<MapScreen> {
           zoom: 14.0,
         ),
       ));
+
+      // Contoh penggunaan geocoding untuk mendapatkan alamat dari koordinat
+      List<Placemark> placemarks = await placemarkFromCoordinates(lat, lng);
+      if (placemarks.isNotEmpty) {
+        Placemark place = placemarks.first;
+        print('Alamat: ${place.street}, ${place.locality}, ${place.country}');
+      }
+    } catch (e) {
+      print("Error displaying prediction: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error: $e")),
+      );
+    }
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      LatLng currentLatLng = LatLng(position.latitude, position.longitude);
+      _addMarker(
+          currentLatLng); // Gunakan hasil dari Geolocator untuk menambah marker
+      mapController.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: currentLatLng,
+          zoom: 14.0,
+        ),
+      ));
+    } catch (e) {
+      print("Error getting current location: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error: $e")),
+      );
     }
   }
 
@@ -101,33 +194,34 @@ class _MapScreenState extends State<MapScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Fake GPS'),
+        title: const Text('KODIR GPS'),
+        backgroundColor: Colors.grey,
         actions: [
           IconButton(
             icon: const Icon(Icons.search),
             onPressed: _handlePressButton,
           ),
+          IconButton(
+            icon: const Icon(Icons.gps_fixed),
+            onPressed: _getCurrentLocation,
+            color: Colors.blue,
+            iconSize: 30,
+          ),
         ],
       ),
       body: GoogleMap(
         onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _initialCenter,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(0,
+              0), // Target awal tidak penting karena akan diganti oleh lokasi aktual
           zoom: 11.0,
         ),
-        markers: _markers,
+        markers: _markers.isNotEmpty ? Set<Marker>.of(_markers) : Set<Marker>(),
         onTap: _onMapTapped,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          mapController.animateCamera(CameraUpdate.newCameraPosition(
-            CameraPosition(
-              target: _currentCenter,
-              zoom: 14.0,
-            ),
-          ));
-        },
-        child: const Icon(Icons.location_searching),
+        onPressed: _startRoute, // Menghubungkan dengan fungsi startRoute
+        child: const Icon(Icons.navigation),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
